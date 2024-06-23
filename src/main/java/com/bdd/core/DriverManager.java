26edd589695f238e7f36dc6d6b9343b494ffe339
LeopoldoIII <nathan.rx@gmail.com>
2024-06-22 19:03:08 -0600

    WebDriverManager

--- src/main/java/com/bdd/core/DriverManager.java
+++ src/main/java/com/bdd/core/DriverManager.java
@@ -1,9 +1,14 @@
 package com.bdd.core;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DriverManager {
     private static WebDriver driver;
@@ -14,10 +19,27 @@ public class DriverManager {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
+            WebDriverManager.chromedriver().setup();
+//            WebDriverManager.chromedriver().browserVersion("126.0.6478.115").setup();
             driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().window().maximize();
         }
         return driver;
+    }
+
+    private static ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--ignore-certificate-errors");
+//        options.setBinary("/Applications/Google Chrome.app/");
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("profile.default_content_settings.popups", 0);
+        prefs.put("download.prompt_for_download", "false");
+        options.setExperimentalOption("prefs", prefs);
+
+        return options;
     }
 
     public static void closeDriver() {
